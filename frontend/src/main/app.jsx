50beef2542cb794a0831dbfdcbae08e21c935939
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-03-18 22:08:45 -0300

    costruindo componentes

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,10 @@
 import '../common/template/dependecies'
 import React from 'react'
-
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sideBar'
 export default props => (
   <div className='wrapper'>
-    <h1>App</h1>
+    <Header />
+    <Sidebar />
   </div>
 )
