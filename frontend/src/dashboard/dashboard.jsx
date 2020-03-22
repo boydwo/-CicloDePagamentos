1053a2cc0c39c54fb284cdeef45c8d46dd6733bb
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-03-22 20:57:27 -0300

    config reducers

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,20 +3,26 @@ import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
+import { connect } from 'react-redux'
 class Dashboard extends Component {
   render() {
+    const { credit, debt } = this.props.summary
     return (
       <div>
         <ContentHeader title='Dashboard' small='Versão 1.0' />
         <Content>
           <Row>
-            <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de Créditos' />
-            <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10' text='Total de Débitos' />
-            <ValueBox cols='12 4' color='purple' icon='money' value='R$ 0' text='Valor Consolidado' />
+            <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de Créditos' />
+            <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de Débitos' />
+            <ValueBox cols='12 4' color='purple' icon='money' value={`R$ ${credit - debt}`} text='Valor Consolidado' />
           </Row>
         </Content >
       </div >
     )
   }
 }
-export default Dashboard
+
+const mapStateToProps = state => ({
+  summary: state.dashboard.summary
+})
+export default connect(mapStateToProps)(Dashboard)
