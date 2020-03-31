cac095013abc0aa3e5a495fd1c2ab602488f934f
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-03-30 22:40:54 -0300

    iniciando tabela

---
+++ frontend/src/billingCycles/billingCycleList.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+class BillingCycleList extends Component {
+
+  render() {
+    return (
+      <div>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Mês</th>
+              <th>Ano</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default BillingCycleList
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycles/billingCycleReducer'
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
-  tab: TabReducer
+  tab: TabReducer,
+  billingCycle: BillingCycleReducer
 })
 export default rootReducer
